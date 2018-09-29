97eece50a0a22688261b0da858a5b989ee6d6e30
Prachi <prachi@HS-14048734.local>
2018-09-29 19:35:03 +0530

    [masters] final commit

--- find_donors.py
+++ find_donors.py
@@ -3,12 +3,12 @@ import numpy as np
 import pandas as pd
 from time import time
 from IPython.display import display # Allows the use of display() for DataFrames
-from IPython import get_ipython
+# Import train_test_split
 from sklearn.cross_validation import train_test_split
 
 
 # Import supplementary visualization code visuals.py
-# import visuals as vs
+import visuals as vs
 # Import sklearn.preprocessing.StandardScaler
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import fbeta_score
@@ -22,29 +22,24 @@ from sklearn.metrics import make_scorer
 import random
 from sklearn.ensemble import AdaBoostClassifier as ABC
 from sklearn.base import clone
-# Import supplementary visualization code visuals.py
-# import vpython as vs
+
 
 # Pretty display for notebooks
-# %matplotlib inline
+%matplotlib inline
 
 # Load the Census dataset
 data = pd.read_csv("census.csv")
 
 # Success - Display the first record
-# plt.show()
-
+display(data.head(n=1))
 
 n_records = data['age'].count()
 df = data['income']
 
-# TODO: Number of records where individual's income is more than $50,000
-n_greater_50k = df[df=='>50'].count()
+n_greater_50k = df[df=='>50K'].count()
 
-# TODO: Number of records where individual's income is at most $50,000
-n_at_most_50k = df[df=='<=50'].count()
+n_at_most_50k = df[df=='<=50K'].count()
 
-# TODO: Percentage of individuals whose income is more than $50,000
 x = n_greater_50k
 y = n_records
 
@@ -55,3 +50,160 @@ print "Total number of records: {}".format(n_records)
 print "Individuals making more than $50,000: {}".format(n_greater_50k)
 print "Individuals making at most $50,000: {}".format(n_at_most_50k)
 print "Percentage of individuals making more than $50,000: {:.2f}%".format(greater_percent)
+
+
+# Split the data into features and target label
+income_raw = data['income']
+features_raw = data.drop('income', axis = 1)
+
+# Visualize skewed continuous features of original data
+vs.distribution(data)
+
+# Log-transform the skewed features
+skewed = ['capital-gain', 'capital-loss']
+features_log_transformed = pd.DataFrame(data = features_raw)
+features_log_transformed[skewed] = features_raw[skewed].apply(lambda x: np.log(x + 1))
+
+# Visualize the new log distributions
+vs.distribution(features_log_transformed, transformed = True)
+
+# Initialize a scaler, then apply it to the features
+scaler = MinMaxScaler() # default=(0, 1)
+numerical = ['age', 'education-num', 'capital-gain', 'capital-loss', 'hours-per-week']
+
+features_log_minmax_transform = pd.DataFrame(data = features_log_transformed)
+features_log_minmax_transform[numerical] = scaler.fit_transform(features_log_transformed[numerical])
+
+# Show an example of a record with scaling applied
+display(features_log_minmax_transform.head(n = 5))
+
+
+features_final = pd.get_dummies(features_log_minmax_transform)
+
+income = income_raw.apply(lambda x: 0 if x == '<=50K' else 1)
+
+# Print the number of features after one-hot encoding
+encoded = list(features_final.columns)
+
+
+# Split the 'features' and 'income' data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(features_final, 
+                                                    income, 
+                                                    test_size = 0.2, 
+                                                    random_state = 0)
+
+# Show the results of the split
+print "Training set has {} samples.".format(X_train.shape[0])
+print "Testing set has {} samples.".format(X_test.shape[0])
+
+
+accuracy = float(n_greater_50k) / (n_greater_50k + n_at_most_50k) 
+
+fscore = 1.25 * (accuracy) / ( 0.25 * accuracy + 1)
+print "Naive Predictor: [Accuracy score: {:.4f}, F-score: {:.4f}]".format(accuracy, fscore)
+
+def train_predict(learner, sample_size, X_train, y_train, X_test, y_test): 
+  
+    results = {}
+    
+    start = time() # Get start time
+    learner = learner.fit(X_train[:sample_size], y_train[:sample_size])
+    end = time() # Get end time
+    
+    results['train_time'] = end - start
+        
+    start = time() # Get start time
+    predictions_test = learner.predict(X_test)
+    predictions_train = learner.predict(X_train[:300])
+    end = time() # Get end time
+    
+    results['pred_time'] = end - start
+            
+    results['acc_train'] = accuracy_score(y_train[:300], predictions_train)
+        
+    results['acc_test'] = accuracy_score(y_test, predictions_test)
+    
+    results['f_train'] = fbeta_score(y_train[:300], predictions_train, 0.5)
+        
+    results['f_test'] = fbeta_score(y_test, predictions_test, 0.5)
+       
+    # Success
+    print "{} trained on {} samples.".format(learner.__class__.__name__, sample_size)
+        
+    # Return the results
+    return results
+
+clf_A = ABC(random_state = 42)
+clf_B = DTC(random_state = 42)
+clf_C = LinearSVC(random_state = 42)
+
+
+samples_1 = len(X_train)/100
+samples_10 = len(X_train)/10
+samples_100 = len(X_train)
+
+
+# Collect results on the learners
+results = {}
+for clf in [clf_A, clf_B, clf_C]:
+        clf_name = clf.__class__.__name__
+        results[clf_name] = {}
+        for i, samples in enumerate([samples_1, samples_10, samples_100]):
+            results[clf_name][i] = \
+            train_predict(clf, samples, X_train, y_train, X_test, y_test)
+
+# Run metrics visualization for the three supervised learning models chosen
+vs.evaluate(results, accuracy, fscore)
+
+lf = ABC()
+
+parameters = {"n_estimators": [1, 50, 100], "learning_rate": [1, 0.5, 0.1]}
+
+scorer = make_scorer(fbeta_score, beta = 0.5)
+
+grid_obj = GridSearchCV(clf, param_grid = parameters, cv = 10, scoring = scorer)
+
+grid_fit = grid_obj.fit(X_train, y_train)
+
+# Get the estimator
+best_clf = grid_fit.best_estimator_
+
+# Make predictions using the unoptimized and model
+predictions = (clf.fit(X_train, y_train)).predict(X_test)
+best_predictions = best_clf.predict(X_test)
+
+# Report the before-and-afterscores
+print "Unoptimized model\n------"
+print "Accuracy score on testing data: {:.4f}".format(accuracy_score(y_test, predictions))
+print "F-score on testing data: {:.4f}".format(fbeta_score(y_test, predictions, beta = 0.5))
+print "\nOptimized Model\n------"
+print "Final accuracy score on the testing data: {:.4f}".format(accuracy_score(y_test, best_predictions))
+print "Final F-score on the testing data: {:.4f}".format(fbeta_score(y_test, best_predictions, beta = 0.5))
+
+
+
+model = ABC()
+model.fit(X_train, y_train)
+
+
+importances = model.feature_importances_
+# Plot the features
+vs.feature_plot(importances, X_train, y_train)
+
+# Reduce the feature space
+X_train_reduced = X_train[X_train.columns.values[(np.argsort(importances)[::-1])[:5]]]
+X_test_reduced = X_test[X_test.columns.values[(np.argsort(importances)[::-1])[:5]]]
+
+# Train on the "best" model found from grid search earlier
+clf = (clone(best_clf)).fit(X_train_reduced, y_train)
+
+# Make new predictions
+reduced_predictions = clf.predict(X_test_reduced)
+
+# Report scores from the final model using both versions of data
+print "Final Model trained on full data\n------"
+print "Accuracy on testing data: {:.4f}".format(accuracy_score(y_test, best_predictions))
+print "F-score on testing data: {:.4f}".format(fbeta_score(y_test, best_predictions, beta = 0.5))
+print "\nFinal Model trained on reduced data\n------"
+print "Accuracy on testing data: {:.4f}".format(accuracy_score(y_test, reduced_predictions))
+print "F-score on testing data: {:.4f}".format(fbeta_score(y_test, reduced_predictions, beta = 0.5))
